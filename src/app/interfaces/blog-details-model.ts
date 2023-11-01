31b434619f8ac22a39bfe49d1b5b030925fc72ba
47thommy <47thommy@gmail.com>
2023-11-01 12:32:31 +0300

    nm

--- src/app/interfaces/blog-details-model.ts
+++
@@ -1,9 +0,0 @@
-import { Subscription } from "rxjs";
-
-export interface BlogDetailsModel {
-    sub: Subscription,
-    error: string,
-    loading: boolean,
-    blogId: string,
-    data: any
-}
--- src/app/interfaces/user.ts
+++
@@ -1,5 +0,0 @@
-export interface User {
-    _id: string,
-    first_name: string,
-    role: string
-}
